19bebe6bae9bfc3ceae5bf72424133728f518bda
kevin <kkovalchik.chusj@gmail.com>
2020-07-06 15:59:46 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Plotly-Logo',
+    version='',
+    packages=['Plotly-Logo'],
+    url='https://github.com/kevinkovalchik/Plotly-Logo',
+    license='',
+    author='Kevin Kovalchik',
+    author_email='',
+    description=''
+)

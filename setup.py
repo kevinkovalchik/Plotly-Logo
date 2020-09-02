df578b327bc4ef8c008e9b1c44bdd30498a1e43e
kevin <kkovalchik.chusj@gmail.com>
2020-09-02 14:26:44 -0400

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='plotly-logo',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     url='https://github.com/kevinkovalchik/Plotly-Logo',
     license='MIT License',
